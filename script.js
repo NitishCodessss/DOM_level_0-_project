dbe02173880aea80698db234d2767a2f36ac4edb
NitishCodessss <nitish304844@gmail.com>
2025-09-10 18:18:33 +0530

    project0_of_DOM

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector('body');
+
+
+buttons.forEach((eve)=>{
+    eve.addEventListener("click",function(button){
+        if(button.target.id === 'grey'){
+            body.style.backgroundColor = button.target.id;
+        }
+         if(button.target.id === 'white'){
+            body.style.backgroundColor = button.target.id;
+        }
+         if(button.target.id === 'blue'){
+            body.style.backgroundColor = button.target.id;
+        }
+         if(button.target.id === 'yellow'){
+            body.style.backgroundColor = button.target.id;
+        }
+
+    });
+});
